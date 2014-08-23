3b939e0c7d643d2c6e6b0663ee527c48074975e2
ben goldenberg <goldenbergben@gmail.com>
2014-08-23 18:03:40 +0300

    updatae

---
+++ js/settings.js
@@ -0,0 +1,47 @@
+define([], function () {
+    
+    var settings = function () {
+    	var dbString='test';
+    	var curl = 'http://app-dev-01.implicit.harvard.edu/implicit/research/library/randomStudiesConfig/RandomStudiesConfig.xml';
+        var hurl = 'http://app-dev-01.implicit.harvard.edu/implicit/research/library/randomStudiesConfig/HistoryRand.xml';
+    	//var db = 'Research';
+    	//var current = 'Any';
+    	var baseURL = 'http://app-dev-01.implicit.harvard.edu/implicit';
+
+    	this.getDbString = function(){
+    		return dbString;
+
+    	}
+    	this.getCurl = function(){
+    		return curl;
+    		
+    	}
+        this.getHurl = function(){
+            return curl;
+            
+        }
+
+    	this.getBaseURL = function(){
+    		return baseURL;
+    		
+    	}
+    	this.setDbString = function(s){
+    		dbString=s;
+
+    	}
+    	this.setCurl = function(s){
+    		curl=s;
+    		
+    	}
+    	this.setBaseURL = function(s){
+    		baseURL=s;
+    		
+    	}
+    	
+
+	};
+
+    return settings;
+        
+});
+       
